107ebeffa2d288f0a06c155d13358181db0c3c6d
maro5397 <lsj000424@gmail.com>
2022-01-12 02:56:06 -0500

    remove

--- vending-machine/send-arp-test/getmyaddr.h
+++
@@ -1,2 +0,0 @@
-int getMacAddress(uint8_t* mac, char* netname);
-int getIPAddress(char* netname);
--- vending-machine/send-arp-test/getmymac.h
+++
@@ -1,3 +0,0 @@
-void convrt_mac(const char *data, char *cvrt_str, int sz);
-int getMacAddress(char *mac, char* netname);
-int getIPAddress(char *ip_addr, char* netname);
